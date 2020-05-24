780714af27a686d5eccbcf2ea6cdce8185271b29
Abhishek Gupta <a@navgurukul.org>
2020-05-25 02:03:32 +0530

    reverting

--- src/dispatcher/payloads.ts
+++ src/dispatcher/payloads.ts
@@ -22,8 +22,6 @@ import { DispatcherAction } from "./actions";
 export interface ActionPayload {
     [property: string]: any; // effectively makes this 'extends Object'
     action: DispatcherAction;
-    register: any;
-    unregister: any;
 }
 
 /**
